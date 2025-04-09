40ed5d68df42a650a34d7f4520a1db89bb2d6b44
SamuelTuv <s.tuvstedt@gmail.com>
2025-04-09 21:56:22 +0200

    begun work on program memory

--- project/project.srcs/sources_1/new/central.v
+++ project/project.srcs/sources_1/new/central.v
@@ -11,8 +11,39 @@
 
 
 module central(
-    input clk
-    
+    input wire clk,
+    input wire reset,
+    output reg [15:0] led
+
     );
+    
+    wire write_enable = 1'b0;
+    wire [15:0] write_addr = 16'b0;
+    wire [31:0] write_data = 16'b0;
+    wire [15:0] read_addr = 32'b0;
+    wire [31:0] read_data = 32'b0;
+        
+    // Instantiate the memory module
+    memory mem_inst (
+        .clk(clk),
+        .reset(reset),
+        .write_enable(write_enable),
+        .write_addr(write_addr),
+        .write_data(write_data),
+        .read_addr(read_addr),
+        .read_data(read_data)
+    );
+    
+    
+    // Always block to assign reset signal to led
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            led <= 16'b0; // Clear LEDs on reset
+        end else begin
+            led <= 16'b1; // Assign reset signal to all LEDs
+        end
+    end
+
+
 endmodule
 
---
+++ project/project.srcs/sources_1/new/memory.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Samuel Tuvstedt
+// 
+// Create Date: 2025-04-08
+// Module Name: central
+// Project Name: Pipeline Pioneer
+// Target Devices: Xilinx FPGA Basys3
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module memory(
+    input wire clk,
+    input wire reset,
+    input wire write_enable,
+    input wire [15:0] write_addr, // only uses 8192 addresses yet so 12:0 is what is needed. 
+    input wire [31:0] write_data,
+    input wire [15:0] read_addr, // only uses 8192 addresses yet so 12:0 is what is needed. 
+    output reg [31:0] read_data
+    );
+    
+    reg [31:0] memory_array [0:255];
+    
+    integer i;
+
+    // Initial program memory
+    initial begin
+        // Example program data
+        memory_array[0] = 32'h00000001;
+        memory_array[1] = 32'h00000002;
+        memory_array[2] = 32'h00000003;
+
+        for (i = 3; i < 255; i = i + 1) begin
+            memory_array[i] <= 32'b0;
+        end
+    end
+
+    
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            read_data <= 32'b0; //
+        end else begin
+            if (write_enable) begin
+                memory_array[write_addr[15:2]] <= write_data;
+            end
+        end 
+            
+        read_data <= memory_array[read_addr[15:2]];
+        end
+endmodule
